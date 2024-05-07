5c19d1d6e20d845e993f824d04c1dec19bd9a1dc
DanyPaisDaSilva <danypaisdasilva@gmail.com>
2024-05-07 12:26:21 +0200

    added basic stat calculation with csv file for untouched dataset

--- data_exploration.py
+++ data_exploration.py
@@ -3,6 +3,9 @@ import time
 
 train_path = "data/training_set_VU_DM.csv"
 
+# for testing:
+# train_path = "data/small_dataset.csv"
+
 start_time = time.time()
 chunk = pd.read_csv(train_path, chunksize=1000000)
 print(f"load chunk: {time.time() - start_time:.2f}s")
@@ -14,6 +17,12 @@ print(f"concat df: {time.time() - start_time:.2f}s")
 # print(df.head())
 # print(df.columns)
 
-for column in df.columns:
-    print(df[column].describe())
+stats = []
 
+for column in df.columns:
+    stats.append(df[column].describe())
+
+df_stats = pd.concat(stats, axis=1).T
+
+stats_path = f"stats_and_graphs/stats/{train_path.split('/')[1][:-4]}_stats.csv"
+df_stats.to_csv(stats_path)
